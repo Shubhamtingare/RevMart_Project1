ceb487ae4bfc7ebc8fdd2d95d0af497e14eb45e8
Shubham Tingare <shubhamtingare0661@gmail.com>
2024-09-13 16:05:07 +0530

    Project Structure updated

---
+++ src/main/java/com/revature/RevMartApplication.java
@@ -0,0 +1,13 @@
+package com.revature;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevMartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RevMartApplication.class, args);
+	}
+
+}
